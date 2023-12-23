4c260caaf26bce3a2e860b455a3ccce5012606aa
Rishikesh2001 <rishikeshpashte23@gmail.com>
2023-12-23 16:33:22 +0530

    Implemented JobDetails section

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,39 +1,36 @@
-import React, { useState } from "react";
-import { TouchableOpacity, FlatList, Text, View } from "react-native";
-
+import React from 'react';
+import { TouchableOpacity, View ,Text, FlatList } from 'react-native'
+import { SIZES } from '../../../constants';
 import styles from "./tabs.style";
-import { SIZES } from "../../../constants";
 
-function TabButton({ name, activeTab, onHandleSearchType }) {
-  return (
-    <TouchableOpacity
-      style={styles.btn(name, activeTab)}
-      onPress={onHandleSearchType}
-    >
-      <Text style={styles.btnText(name, activeTab)}>{name}</Text>
-    </TouchableOpacity>
-  );
-}
+const TabButton = ({ name, activeTab, onHandleSearchType }) => (
+  <TouchableOpacity
+    style={styles.btn(name, activeTab)}
+    onPress={onHandleSearchType}
+  >
+    <Text style={styles.btnText(name, activeTab)}>{name}</Text>
+  </TouchableOpacity>
+);
 
-const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+const JobTabs = ({tabs, activeTab, setActiveTab}) => {
   return (
     <View style={styles.container}>
-      <FlatList
+      <FlatList 
         data={tabs}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <TabButton
+        renderItem={(item) => (
+          <TabButton 
             name={item}
             activeTab={activeTab}
             onHandleSearchType={() => setActiveTab(item)}
           />
         )}
-        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
-        keyExtractor={(item) => item}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={item => item}
+        contentContainerStyle={{ columnGap: SIZES.small/2 }}
       />
     </View>
-  );
-};
+  )
+}
 
-export default Tabs;
+export default JobTabs;
